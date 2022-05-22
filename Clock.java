263bcbd4ee08f64022625c8ecf3ab15750bd9048
MohamedTahaBakr <100281859+MohamedTahaBakr@users.noreply.github.com>
2022-05-22 05:38:43 +0200

    Create Clock.java

---
+++ Clock.java
@@ -0,0 +1,14 @@
+package clock;
+
+public class Clock {
+
+    
+    public static void main(String[] args) {
+       
+        
+     System.out.println("My name is mohamed bakr ");
+        new myFrame();
+        
+    }
+    
+}
